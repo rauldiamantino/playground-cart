e30154b7ebc60319adb28c83a88c9eb6b1ee106c
Raul Diamantino <rauldiamantino25@gmail.com>
2026-03-14 02:43:56 -0300

    feat: multi-tenancy initial structury and migration configurations separated

---
+++ routes/tenant.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+
+/*
+|--------------------------------------------------------------------------
+| Tenant Routes
+|--------------------------------------------------------------------------
+|
+| Here you can register the tenant routes for your application.
+| These routes are loaded by the TenantRouteServiceProvider.
+|
+| Feel free to customize them however you want. Good luck!
+|
+*/
+
+Route::middleware([
+    'web',
+    InitializeTenancyByDomain::class,
+    PreventAccessFromCentralDomains::class,
+])->group(function () {
+    Route::get('/', function () {
+        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+    });
+
+    Route::get('/hello', function () {
+        return 'Hello, welcome to your store!';
+    });
+
+    Route::get('/db-debug', function () {
+        return [
+            'connection_name' => DB::getDefaultConnection(),
+            'database_name' => DB::connection()->getDatabaseName(),
+            'tenant_id' => tenant('id'),
+        ];
+    });
+});
