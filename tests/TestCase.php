0be44e7e4d91c2e203c4678d24253594fe91e532
Raul Diamantino <rauldiamantino25@gmail.com>
2026-03-16 00:31:54 -0300

    fix: remove comments

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Platform\Models\Tenant;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -10,8 +11,6 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
 
-        // Desativa os eventos do model Tenant para que ele não tente
-        // disparar a criação de banco físico no MySQL durante os testes.
-        \App\Platform\Models\Tenant::flushEventListeners();
+        Tenant::flushEventListeners();
     }
 }
